e06e5367d38e57877a4229b81926cff53a6ec1c0
Faizan Qureshi <faizan.qureshi@paymentology.com>
2025-12-10 12:30:40 -0500

    changes

--- bootstrap-netw-non-prod.tf
+++ bootstrap-netw-non-prod.tf
@@ -2,7 +2,7 @@
 
 
 # Module 1: OIDC Identity Provider for dev account 
-module "oidc_provider-network" {
+module "oidc_provider-non-prod-network" {
   source = "./modules/oidc"
   providers = {
     aws = aws.fq-netw-non-prod
@@ -12,20 +12,20 @@ module "oidc_provider-network" {
 
 
 # creating all the policies in to the dev account 
-module "policies-network" {
+module "policies-non-prod-network" {
   source        = "./modules/policies"
-  s3_bucket_arn = module.terraform_state-network.s3_bucket_name
+  s3_bucket_arn = module.terraform_state-non-prod-network.s3_bucket_name
   providers = {
     aws = aws.fq-netw-non-prod
   }
 }
 
 # Module 2: IAM Role Creation for foundation role into the dev account
-module "role_foundation-network" {
+module "role_foundation--non-prod-network" {
   source              = "./modules/iam_role"
   role_name           = "foundation-role"
-  assume_role_policy  = data.aws_iam_policy_document.github_assume_role_policy-dev.json
-  managed_policy_arns = [module.policies-network.fq_foundation_write, module.policies-network.tfstate_write, "arn:aws:iam::aws:policy/AmazonS3FullAccess", "arn:aws:iam::aws:policy/IAMAccessAnalyzerFullAccess", "arn:aws:iam::aws:policy/ServiceQuotasFullAccess"]
+  assume_role_policy  = data.aws_iam_policy_document.github_assume_role_policy-non-prod-network.json
+  managed_policy_arns = [module.policies-non-prod-network.fq_foundation_write, module.policies-non-prod-network.tfstate_write, "arn:aws:iam::aws:policy/AmazonS3FullAccess", "arn:aws:iam::aws:policy/IAMAccessAnalyzerFullAccess", "arn:aws:iam::aws:policy/ServiceQuotasFullAccess"]
   providers = {
     aws = aws.fq-netw-non-prod
   }
@@ -33,12 +33,12 @@ module "role_foundation-network" {
 
 
 
-module "terraform_state-network" {
+module "terraform_state-non-prod-network" {
   source        = "./modules/terraform_state"
   bucket_name   = "fq-network-terraform-state-bucket"
   dynom_db_name = "state_dynamo_db"
   tags = { "Name" : "terraform_state",
-  "environment" : "dev" }
+  "environment" : "non-prod-network" }
   providers = {
     aws = aws.fq-netw-non-prod
   }
--- data.tf
+++ data.tf
@@ -25,7 +25,7 @@ data "aws_iam_policy_document" "github_assume_role_policy-dev" {
 
 
 #trust policy this is just the read trust policy nothing created can attach it to any role
-data "aws_iam_policy_document" "github_assume_role_policy-network" {
+data "aws_iam_policy_document" "github_assume_role_policy-non-prod-network" {
   statement {
     actions = ["sts:AssumeRoleWithWebIdentity"]
 
@@ -43,9 +43,61 @@ data "aws_iam_policy_document" "github_assume_role_policy-network" {
 
     principals {
       type        = "Federated"
-      identifiers = [module.oidc_provider-network.oidc_provider_arn]
+      identifiers = [module.oidc_provider-non-prod-network.oidc_provider_arn]
     }
   }
-  provider = aws.fq-personal-network
+  provider = aws.fq-netw-non-prod
+
+}
+
+
+data "aws_iam_policy_document" "github_assume_role_policy-network-prod" {
+  statement {
+    actions = ["sts:AssumeRoleWithWebIdentity"]
+
+    condition {
+      test     = "StringEquals"
+      variable = "token.actions.githubusercontent.com:aud"
+      values   = ["sts.amazonaws.com"]
+    }
+
+    condition {
+      test     = "StringLike"
+      variable = "token.actions.githubusercontent.com:sub"
+      values   = var.github_repos
+    }
+
+    principals {
+      type        = "Federated"
+      identifiers = [module.oidc_provider-network-prod.oidc_provider_arn]
+    }
+  }
+  provider = aws.fq-netw-prod
+
+}
+
+
+data "aws_iam_policy_document" "github_assume_role_policy-mgmt" {
+  statement {
+    actions = ["sts:AssumeRoleWithWebIdentity"]
+
+    condition {
+      test     = "StringEquals"
+      variable = "token.actions.githubusercontent.com:aud"
+      values   = ["sts.amazonaws.com"]
+    }
+
+    condition {
+      test     = "StringLike"
+      variable = "token.actions.githubusercontent.com:sub"
+      values   = var.github_repos
+    }
+
+    principals {
+      type        = "Federated"
+      identifiers = [module.oidc_provider-mgmt.oidc_provider_arn]
+    }
+  }
+  provider = aws.AWSMGMT
 
 }
